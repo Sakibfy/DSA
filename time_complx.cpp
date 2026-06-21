70321ae7d23179046e531f756f89dd402ef5b797
Md Sakib <gameingsakib71@gmail.com>
2026-06-21 23:39:50 +0600

    logarithmic Complexity

--- time_complx.cpp
+++ time_complx.cpp
@@ -3,9 +3,22 @@ using namespace std;
 
 int main(){
 
-   
+   int n;
+ cin >> n;
 
-   cout << "hello c++";
+ for (int i = 1; i < n; i*=3)
+ {
+   cout << i << endl;
+ }
+
+ cout << endl;
+ 
+ for (int i = n; i >= 1; i/=3)
+ {
+   cout << i << endl;
+ }
+
+  
 
     return 0;
 }
